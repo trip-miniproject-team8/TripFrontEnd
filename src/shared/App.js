42af39680635d9d08aaea1bde594b89f762a2c52
parkksss <yesleee9211@gmail.com>
2022-04-08 17:39:54 +0900

    shared 폴더 components 추가

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,25 +1,54 @@
-import logo from '../logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "../redux/configureStore";
+import {useDispatch} from "react-redux";
+import {actionCreators as userActions} from "../redux/modules/user";
+
+import {apiKey} from "./firebase";
+
+import PostList from '../pages/PostList';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+import Header from '../components/Header';
+import { Grid, Button } from '../elements';
+import Permit from './Permit';
+import PostWrite from '../pages/PostWrite';
+import PostDetail from '../pages/PostDetail';
+// import Notification from '../pages/Notification';
+
 
 function App() {
+  const dispatch = useDispatch();
+  const _session_key = `firebase:authUser:${apiKey}:[DEFAULT]`;
+  const is_session = sessionStorage.getItem(_session_key) ? true : false;
+
+  React.useEffect(() => {
+    if (is_session) {
+      dispatch(userActions.loginCheckFB());
+    }
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Grid>
+        <Header></Header>
+        <ConnectedRouter history={history}>
+          <Route path="/" exact component={PostList} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/posting" exact component={PostWrite}/>
+          <Route path="/posting/:id" exact component={PostWrite}/>
+          <Route path="/post/:id" exact component={PostDetail} />
+          {/* <Route path="/noti" exact component={Notification}/> */}
+        </ConnectedRouter>
+      </Grid> 
+      <Permit>
+        <Button is_float text='+' _onClick={() => {history.push('/posting');}}></Button>
+      </Permit>
+    </React.Fragment>
+    );
 }
 
 export default App;
---
+++ src/shared/Upload.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button } from "../elements";
+import { storage } from './firebase';
+
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreators as imageActions } from "../redux/modules/image";
+
+const Upload = (props) => {
+    const dispatch = useDispatch();
+    const is_uploading = useSelector((state) => state.image.uploading);
+    const fileInput = React.useRef();
+
+    const selectFile = (e) => {
+
+        const reader = new FileReader();
+        const file = fileInput.current.files[0];
+
+        reader.readAsDataURL(file);
+        console.log('file :: ', file)
+        console.log('reader ::', reader)
+
+        reader.onloadend = () => {
+            console.log('reader.result :: ', reader.result);
+            dispatch(imageActions.setPreview(reader.result));
+        };
+    }
+
+    const uploadFB = () => {
+        let image = fileInput.current.files[0];
+        
+        dispatch(imageActions.uploadImageFB(image));
+    }
+
+    return (
+        <React.Fragment>
+            <input onChange={selectFile} type="file" ref={fileInput} disabled={is_uploading}/>
+            {/* <Button _onClick={uploadFB}>업로드하기</Button> */}
+        </React.Fragment>
+    )
+}
+
+export default Upload;
